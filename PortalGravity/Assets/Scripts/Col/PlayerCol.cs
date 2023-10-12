b4fbcde90d5c9ba4982b527f8872e7ccae558fc6
higashihokakeru <kakeru.0422.pojr@gmail.com>
2023-10-12 16:31:58 +0900

    Update 移動とジャンプを実装

--- PortalGravity/Assets/Scripts/Col/PlayerCol.cs
+++ PortalGravity/Assets/Scripts/Col/PlayerCol.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerCol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private PlayerController player = default;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other) 
     {
-        
+        player ??= this.GetComponent<PlayerController>();
+            
+
+        if(other.gameObject.name == "yuka" && !player.IsGrounded)
+        {
+            //playerの下で当たっていたら
+            if(MethodFactory.GetColDir(other) == Enums.ColDir.DOWN)
+            {
+                Debug.Log("in");
+                player.IsGrounded = !player.IsGrounded;
+            }
+        }
     }
 }
